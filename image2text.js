5c1e835d24a44feae74679aaba3855e69067fea5
SonnyTaylor <santinotaylor08@gmail.com>
2024-07-16 20:58:46 +1000

    setup image2text for later

---
+++ image2text.js
@@ -0,0 +1,25 @@
+class ImageAccessibilityEnhancer {
+    getAllImagesWithMissingAltTag() {
+        // Logic to get all images without an alt tag
+    }
+
+    convertImageToHash() {
+        // Logic to convert image to hash
+    }
+
+    addImageHashToBrowserStorage() {
+        // Logic to add image hash to browser storage
+    }
+
+    requestGPTToConvertImageToText() {
+        // Logic to request GPT to convert image to text
+    }
+
+    addImageTagToCorrespondingHash() {
+        // Logic to add image tag to corresponding hash
+    }
+
+    replaceOrAddImageDescriptionToAltTag() {
+        // Logic to replace or add image description to alt tag
+    }
+}
